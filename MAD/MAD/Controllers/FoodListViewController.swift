8761d96cda0cb172515dffee3191bd608f58f9a8
Michael Maxner <mmaxner@rogers.com>
2018-10-01 18:23:42 -0400

    TableView stuff

--- MAD/MAD/Controllers/FoodListViewController.swift
+++ MAD/MAD/Controllers/FoodListViewController.swift
@@ -3,20 +3,51 @@
 //  MAD
 //
 //  Created by Connor Henry on 2018-10-01.
-//  Copyright © 2018 Conner Henry. All rights reserved.
+//  Copyright © 2018 Not Conner Henry. All rights reserved.
 //
 
 import UIKit
 
-class FoodListViewController: UIViewController {
+class FoodListViewController: UIViewController, UITableViewDataSource  {
+    let sections = ["fruit", "vegetals", "very bad"];
+    let fruits = ["banana", "orang", "tomato"];
+    let vegetals = ["cucumber", "brocoli", "carrot"];
+    let very_bad = ["steak", "burger", "hotdog"];
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section];
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count;
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch (section) {
+        case 0:
+            return fruits.count;
+        case 1:
+            return vegetals.count;
+        case 2:
+            return very_bad.count;
+        default:
+            return 0;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlainCell", for: indexPath);
+        switch (indexPath.section) {
+        case 0:
+            cell.textLabel?.text = fruits[indexPath.row];
+        case 1:
+            cell.textLabel?.text = vegetals[indexPath.row];
+        case 2:
+            cell.textLabel?.text = very_bad[indexPath.row];
+        default:
+            break;
+        }
+        return cell;
+    }
+    
 }
